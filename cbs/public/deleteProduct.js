0a489b03139f8b58685cf7f5746e0421dc2200d0
Andrew <andrew-yao@users.noreply.github.com>
2020-08-03 15:13:56 -0700

    removed andrew's cbs_coffee setup
    removed andrew's cbs_coffee files, setup in side branch
    
    designated jordan's /cbs as master

--- cbs/public/deleteProduct.js
+++
@@ -1,9 +0,0 @@
-function deleteProduct(id){
-    $.ajax({
-        url: '/products/' + id,
-        type: 'DELETE',
-        success: function(result){
-            window.location.reload(true);
-        }
-    })
-};
--- main.js
+++ main.js
@@ -1,16 +1,13 @@
-/*
-    Uses express, dbcon for database connection, body parser to parse form data
-    handlebars for HTML templates
-*/
+const express = require('express');
+const mysql = require('./db/config.js');
+const bodyParser = require('body-parser');
+const handlebars = require('express-handlebars').create({
+        defaultLayout:'main'
+    });
 
-var express = require('express');
-var mysql = require('./dbcon.js');
-var bodyParser = require('body-parser');
+const jQuery = require('jquery');
 
-var app = express();
-var handlebars = require('express-handlebars').create({
-        defaultLayout:'main',
-        });
+const app = express();
 
 app.engine('handlebars', handlebars.engine);
 app.use(bodyParser.urlencoded({extended:true}));
@@ -18,23 +15,38 @@ app.use('/static', express.static('public'));
 app.set('view engine', 'handlebars');
 app.set('port', process.argv[2]);
 app.set('mysql', mysql);
+
+
+// Home/Index page:
+app.get('/', function(req,res){
+    res.render('home');
+});
+app.get('/index', function(req,res){
+    res.render('home');
+});
+
+// app.use('/', express.static('public'));
 app.use('/products', require('./products.js'));
 app.use('/farms', require('./farms.js'));
 app.use('/customers', require('./customers.js'));
 app.use('/orders', require('./orders.js'));
-app.use('/', express.static('public'));
 
+
+
+
+// Errors:
 app.use(function(req,res){
-  res.status(404);
-  res.render('404');
+    res.status(404);
+    res.render('404');
 });
-
+  
 app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500);
-  res.render('500');
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
 });
-
+  
 app.listen(app.get('port'), function(){
-  console.log('Express started on port ' + app.get('port') + '; press Ctrl-C to terminate.');
+    console.log('Express started on port ' + app.get('port') + '; press Ctrl-C to terminate.');
 });
+  
