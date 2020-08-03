46893b18e6aad21c5720f28c711a0138907304d5
Andrew <andrew-yao@users.noreply.github.com>
2020-08-03 15:26:50 -0700

    removed cbs\

--- cbs/public/searchCustomers.js
+++
@@ -1,5 +0,0 @@
-function getCustomersByName(){
-    var searchFName = document.getElementById('search-fname').value;
-    //construct the URL and redirect to it
-    window.location = '/people/search/' + encodeURI(searchFName);
-}
